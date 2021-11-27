3050479d2ada7e4d65e7f33c42f2d50f0e3d8e09
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-11-27 23:15:06 +0700

    feat: add outline btn

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -5,17 +5,23 @@ import styles from './Button.module.scss';
 interface ButtonProps {
   children: string;
   color: 'cyan' | 'red-soil' | 'dark-green' | 'nude' | 'black' | 'white';
-  mode?: 'fill' | 'contain';
+  mode?: 'fill-parent' | 'contain';
+  variant?: 'fill' | 'outline';
 }
 
 const Button: React.FC<ButtonProps> = ({
   mode = 'contain',
+  variant = 'fill',
   color,
   children,
 }: ButtonProps) => {
   return (
     <button
-      className={c([styles.btn, styles[`btn-${mode}`], styles[`btn-${color}`]])}
+      className={c([
+        styles.btn,
+        styles[`btn-${mode}`],
+        styles[`btn-${variant}-${color}`],
+      ])}
     >
       {children}
     </button>
