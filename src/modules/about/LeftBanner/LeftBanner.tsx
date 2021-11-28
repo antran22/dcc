eccdb60d8360063e5e8d0ae556ffcff3b2e7840b
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-11-28 11:44:19 +0700

    feat: add banner bg

--- src/modules/about/LeftBanner/LeftBanner.tsx
+++ src/modules/about/LeftBanner/LeftBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './LeftBanner.module.scss';
 
 const LeftBanner: React.FC = () => {
-  return <div className={styles['left-banner']}>Left banner bro</div>;
+  return <div className={styles['left-banner']}></div>;
 };
 
 export default LeftBanner;
