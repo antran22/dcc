7d934864ab6fba88c0d0545ad528adf2273ac923
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-11-28 07:19:32 +0700

    feat: add about banner to assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,15 @@
+// IMG
+import aboutBanner from './img/about-banner.png';
+
+// SVG
 import dccFullLong from './svg/dcc-full-long.svg';
 import logo from './svg/logo.svg';
 
 export const assets = {
+  // IMG,
+  aboutBanner,
+
+  // SVG
   dccFullLong,
   logo,
 };
