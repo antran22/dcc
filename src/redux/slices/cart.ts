e0f14d6669fa8d5b7b91c9bdbabeb1122f1b152b
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-12-06 14:42:54 +0700

    feat: add cart selectors

--- src/redux/slices/cart.ts
+++ src/redux/slices/cart.ts
@@ -1,20 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { CartItem } from '../../shared/types';
+import { RootState } from '../store';
 
-interface CartState {
-  cart: CartItem[];
-}
-
-const initialState: CartState = {
-  cart: [],
-};
+const initialState: CartItem[] = [];
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addItem: (state, action) => {
-      state.cart.push(action.payload);
+      state.push(action.payload);
     },
   },
 });
+
+export const { addItem } = cartSlice.actions;
+export const cartSelector = (state: RootState) => state.cart;
+export const cartAmountSelector = (state: RootState) => state.cart.length;
