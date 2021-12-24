e5ac62b31031ee828b55cb522d991938fe4a0537
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-12-24 11:51:54 +0700

    chore: clean up plop files

--- plopfile.js
+++ plopfile.js
@@ -1,35 +1,6 @@
+const { createComponent } = require('./plop-templates/generators');
+
 module.exports = function (plop) {
   // create your generators here
-  plop.setGenerator('create-component', {
-    description: 'Create a component',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Component name (CamelCase): ',
-      },
-      {
-        type: 'input',
-        name: 'directory',
-        message: 'Component directory: ',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/{{directory}}/{{name}}/{{name}}.tsx',
-        templateFile: 'plop-templates/component/Component.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/{{directory}}/{{name}}/index.ts',
-        templateFile: 'plop-templates/component/index.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/{{directory}}/{{name}}/{{name}}.module.scss',
-        templateFile: 'plop-templates/component/Style.hbs',
-      },
-    ],
-  });
+  plop.setGenerator('create-component', createComponent);
 };
