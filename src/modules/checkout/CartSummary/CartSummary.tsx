0be769cfade6afaffd2ea57a43f233e1dbf31260
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-12-24 11:10:23 +0700

    feat: add cart summary component

---
+++ src/modules/checkout/CartSummary/CartSummary.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './CartSummary.module.scss';
+
+const CartSummary: React.FC = () => {
+  return <div className={styles['cart-summary']}>Cart Summary</div>;
+};
+
+export default CartSummary;
--- src/modules/checkout/CheckoutPage.tsx
+++ src/modules/checkout/CheckoutPage.tsx
@@ -1,12 +1,20 @@
 import { NextPage } from 'next';
 import React from 'react';
+import CartSummary from './CartSummary';
 import styles from './CheckoutPage.module.scss';
 import Footer from './Footer';
 
 const CheckoutPage: NextPage = () => {
   return (
     <div className={styles['checkout-page']}>
-      <div className={styles['checkout-page-content']}>Content</div>
+      <div className={styles['checkout-page-content']}>
+        <section className={styles['checkout-page-content-section']}>
+          form
+        </section>
+        <section className={styles['checkout-page-content-section']}>
+          <CartSummary />
+        </section>
+      </div>
       <Footer />
     </div>
   );
