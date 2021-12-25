45108af3d9a1bd9397c36ebc5c96ee6ee2fa9f08
Le Thanh An <lethanhanzzz@gmail.com>
2021-12-25 20:40:49 +0700

    feat: add cross sell banner

--- src/modules/cart/CrossSell/CrossSell.tsx
+++ src/modules/cart/CrossSell/CrossSell.tsx
@@ -8,12 +8,13 @@ import Spacer from '../../../shared/components/Spacer';
 interface CrossSellProps {}
 
 const CrossSell: React.FC<CrossSellProps> = ({}) => {
-  const crossSellItem: CartItem = {
-    id: String(Math.random()),
+  const crossSellItems: CartItem[] = [...Array(2)].map((_, i) => ({
+    id: String(i * 20),
     name: 'Thiep',
     price: 30000,
     quantity: 1,
-  };
+  }));
+
   return (
     <div className={styles['cross-sell']}>
       <Text.P thickness="thin" classNames={[styles['cross-sell-p']]}>
@@ -21,9 +22,9 @@ const CrossSell: React.FC<CrossSellProps> = ({}) => {
       </Text.P>
 
       <div className={styles['cross-sell-items-wrapper']}>
-        <CrossSellItem crossSellItem={crossSellItem} />
-        <Spacer size="big" />
-        <CrossSellItem crossSellItem={crossSellItem} />
+        {crossSellItems.map((item) => (
+          <CrossSellItem key={item.id} crossSellItem={item} />
+        ))}
       </div>
     </div>
   );
--- src/modules/cart/CrossSellItem/CrossSellItem.tsx
+++ src/modules/cart/CrossSellItem/CrossSellItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../../shared/components/Button';
 import Text from '../../../shared/components/Text';
 import { CartItem } from '../../../shared/types';
@@ -6,13 +6,27 @@ import { formatCurrency } from '../../../shared/utils/number';
 import styles from './CrossSellItem.module.scss';
 import Image from 'next/image';
 import { assets } from '../../../assets';
+import { useAppDispatch } from '../../../redux/hooks';
+import { addItem, removeItem } from '../../../redux/slices/cart';
 
 interface CrossSellItemProps {
   crossSellItem: CartItem;
 }
 
 const CrossSellItem: React.FC<CrossSellItemProps> = ({ crossSellItem }) => {
+  const dispatch = useAppDispatch();
+  const [isItemAdded, setIsItemAdded] = useState(false);
   const { name, price } = crossSellItem;
+
+  const handleCtaClick = () => {
+    if (isItemAdded) {
+      dispatch(removeItem(crossSellItem.id));
+      setIsItemAdded(false);
+    } else {
+      dispatch(addItem(crossSellItem));
+      setIsItemAdded(true);
+    }
+  };
 
   return (
     <div className={styles['cross-sell-item']}>
@@ -26,8 +40,12 @@ const CrossSellItem: React.FC<CrossSellItemProps> = ({ crossSellItem }) => {
       </div>
 
       <div className={styles['cross-sell-item-button']}>
-        <Button mode="fill-parent" color="black">
-          THEM
+        <Button
+          onClick={handleCtaClick}
+          mode="fill-parent"
+          color={isItemAdded ? 'cyan' : 'black'}
+        >
+          {isItemAdded ? 'DA THEM' : 'THEM'}
         </Button>
       </div>
     </div>
